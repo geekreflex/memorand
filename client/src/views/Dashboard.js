cd980c194584fb18fbec9294791cb8a0b7df9056
joshnwosu <joshuanwosu078@gmail.com>
2021-07-21 14:48:06 +0100

    checked if user.firstname exists

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -4,9 +4,11 @@ import { useSelector } from "react-redux";
 const Dashboard = () => {
   const user = useSelector((state) => state.user.user);
 
+  console.log(user);
+
   return (
     <div>
-      <h1>{user.firstName.toUpperCase()} Dashboard</h1>
+      <h1>{user.firstName?.toUpperCase()} Dashboard</h1>
     </div>
   );
 };
