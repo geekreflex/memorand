38f20ae3068d59bcd1cb770c0bad06ae428b2c3f
jerry <jerrynwosu007@gmail.com>
2021-09-18 14:24:27 +0100

    dotenv issue

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
