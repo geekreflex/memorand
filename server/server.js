466517a878b13f812af0854ce8ad4b16f470ec78
jerry <jerrynwosu007@gmail.com>
2021-09-18 13:19:53 +0100

    setup static file

--- server/server.js
+++ server/server.js
@@ -1,21 +1,36 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
-const connectDB = require("./config/db");
-const userRoute = require("./routes/userRoute");
-const noteRoute = require("./routes/noteRoute");
-const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const connectDB = require('./config/db');
+const userRoute = require('./routes/userRoute');
+const noteRoute = require('./routes/noteRoute');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 connectDB();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoute);
-app.use("/api/notes", noteRoute);
+app.use('/api/users', userRoute);
+app.use('/api/notes', noteRoute);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+  app.use(cors());
+}
 
 app.use(notFound);
 app.use(errorHandler);
